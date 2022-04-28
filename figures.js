1ab7d0dc080796768183cf7a0b179d4e7385b21f
Jorge Leon <jorgeleonpicture@gmail.com>
2022-04-28 14:57:03 +0200

    added button functionality

--- figures.js
+++ figures.js
@@ -50,3 +50,61 @@ function circleArea(radio) {
 }
 
 console.groupEnd();
+
+// Square functions
+
+function calculateSquarePerimeter() {
+  const input = document.getElementById("squareInput");
+  const value = input.value;
+  const perimeter = squarePerimeter(value);
+  alert(perimeter);
+}
+
+function calculateSquareArea() {
+  const input = document.getElementById("squareInput");
+  const value = input.value;
+  const area = squareArea(parseInt(value));
+  alert(area);
+}
+
+// Triangle functions
+
+function calculateTrianglePerimeter() {
+  const input1 = document.getElementById("triangleSide1Input");
+  const input2 = document.getElementById("triangleSide2Input");
+  const input3 = document.getElementById("triangleBaseInput");
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const value3 = input3.value;
+  const perimeter = trianglePerimeter(
+    parseInt(value1),
+    parseInt(value2),
+    parseInt(value3)
+  );
+  alert(perimeter);
+}
+
+function calculateTriangleArea() {
+  const input1 = document.getElementById("triangleHeightInput");
+  const input2 = document.getElementById("triangleBase2Input");
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const area = triangleArea(parseInt(value1), parseInt(value2));
+  alert(area);
+}
+
+// Circle functions
+
+function calculateCirclePerimeter() {
+  const input = document.getElementById("circleInput");
+  const value = input.value;
+  const perimeter = circlePerimeter(value);
+  alert(perimeter);
+}
+
+function calculateCircleArea() {
+  const input = document.getElementById("circleInput");
+  const value = input.value;
+  const area = circleArea(value);
+  alert(area);
+}
